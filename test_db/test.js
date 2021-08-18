664e13906dd1abf1f260bfa3f05c32d24632dbfe
riffAt2013 <rifat.masud@northsouth.edu>
2021-08-18 13:20:30 +0600

    rehash

--- test_db/test.js
+++ test_db/test.js
@@ -1,13 +1,13 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
-    apiKey: "AIzaSyBnGMPSO546hpwnIqhWyMXcRweY6PuzaAU",
-    authDomain: "cse499a-dff5d.firebaseapp.com",
-    projectId: "cse499a-dff5d",
-    storageBucket: "cse499a-dff5d.appspot.com",
-    messagingSenderId: "376721390723",
-    appId: "1:376721390723:web:bb97a93b72f196057f2fb6",
-    measurementId: "G-FWCDFEQ3XX"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
