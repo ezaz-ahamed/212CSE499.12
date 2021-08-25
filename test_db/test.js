afd7d1993b7ec15ea7cb03b32a4331b56f357897
riffAt2013 <rifat.masud@northsouth.edu>
2021-08-26 02:01:12 +0600

    maps and database demo completed

--- test_db/test.js
+++ test_db/test.js
@@ -1,19 +1,19 @@
 // creates user as a json, in users "collections" in NOSQL FormData, adds IDBTransaction, 
 // conlogs the id
 
-var firebaseConfig = {
 
-};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 var db = firebase.firestore();
 
-function add(collection_name, first, last, born) {
+function add(collection_name, first, last, uname, password, email) {
     db.collection(collection_name).add({
         first: first,
         last: last,
-        born: Number(born)
+        username: uname,
+        password: password,
+        email: email,
     }).then(function () {
         console.log("Status Saved");
     }).catch((error) => {
@@ -25,10 +25,12 @@ function add(collection_name, first, last, born) {
 
 document.getElementById("myBtn").addEventListener("click", function () {
     const fname = document.getElementById("fname").value;
-    console.log(fname);
     const lname = document.getElementById("lname").value;
-    const bdate = document.getElementById("bdate").value;
-    add("users", fname, lname, bdate);
+    const uname = document.getElementById("uname").value;
+    const pass = document.getElementById("pass").value;
+    const email = document.getElementById("email").value;
+
+    add("users", fname, lname, uname, pass, email);
 
     setTimeout(() => {
         window.open("./success.html");
