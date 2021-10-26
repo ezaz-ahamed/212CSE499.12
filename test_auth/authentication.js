abd7968c0d1896877cbcdd118571223e9483ac53
riffAt2013 <rifat.masud@northsouth.edu>
2021-10-27 01:11:06 +0600

    authentication complete, UI toggle on route

--- test_auth/authentication.js
+++ test_auth/authentication.js
@@ -9,3 +9,62 @@ var firebaseConfig = {
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth()
+
+
+auth.onAuthStateChanged((user) => {
+    if (user) {
+        workerOnAuth(user)
+    } else {
+        workerOnAuth(user)
+        console.log('User is logged Out');
+    }
+});
+
+// sign-up
+const signupForm = document.querySelector('#signup-form')
+signupForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const email = signupForm['signup-email'].value
+    const password = signupForm['signup-password'].value
+
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+        console.log(cred.user);
+        // close the signup modal & reset form
+        const modal = document.querySelector('#modal-signup')
+        M.Modal.getInstance(modal).close()
+        signupForm.reset()
+    }).catch((e) => {
+        signupForm.reset()
+    })
+})
+
+// login
+const loginForm = document.querySelector('#login-form')
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    // get user info
+    const email = loginForm['login-email'].value
+    const password = loginForm['login-password'].value
+    // log the user in
+    auth.signInWithEmailAndPassword(email, password).then((cred) => {
+        console.log(cred.user);
+        // close the signup modal & reset form
+        const modal = document.querySelector('#modal-login')
+        M.Modal.getInstance(modal).close()
+        loginForm.reset()
+    }).catch((e) => {
+        loginForm.reset()
+    })
+})
+
+
+// logout
+const logout = document.querySelector('#logout')
+logout.addEventListener('click', (e) => {
+    auth.signOut()
+    location.reload()
+})
