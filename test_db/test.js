91e0c32cdaca81669aef735d7afc867203e0d152
riffAt2013 <rifat.masud@northsouth.edu>
2021-11-02 23:48:10 +0600

    manual voting demo

--- test_db/test.js
+++ test_db/test.js
@@ -2,8 +2,15 @@
 // conlogs the id
 
 var firebaseConfig = {
-
+    apiKey: "AIzaSyBnGMPSO546hpwnIqhWyMXcRweY6PuzaAU",
+    authDomain: "cse499a-dff5d.firebaseapp.com",
+    projectId: "cse499a-dff5d",
+    storageBucket: "cse499a-dff5d.appspot.com",
+    messagingSenderId: "376721390723",
+    appId: "1:376721390723:web:bb97a93b72f196057f2fb6",
+    measurementId: "G-FWCDFEQ3XX"
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
